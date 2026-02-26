fa0b0193671e90a7671b7f4f19ea2d7e5b8fa7bc
kaxixi6666 <xia.sisi.xv0@g.ext.naist.jp>
2026-02-27 02:50:46 +0900

    Add global exception handler to catch invalid data format errors

---
+++ backend/src/main/java/com/foodflow/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.foodflow.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<String> handleInvalidFormatException(InvalidFormatException ex) {
+        System.err.println("Invalid format exception: " + ex.getMessage());
+        ex.printStackTrace();
+        return ResponseEntity.badRequest().body("Invalid data format: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        System.err.println("General exception: " + ex.getMessage());
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    }
+}
